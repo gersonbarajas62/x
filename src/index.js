3ca2ae5a886e6ebd78226c22e087baa8c2d949e8
gersonbarajas62 <gersonbarajas62@gmail.com>
2019-03-29 00:29:41 -0600

    creating the props

--- src/index.js
+++ src/index.js
@@ -1,29 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
   return (
-    <div className="ui container comments"> 
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" />  
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            Sam
-          </a>
-          <div className="metadata">
-            <span className="date">Today at 6:00PM</span>  
-          </div>
-          <div className="text"></div>
-        </div>    
-      </div>
+    <div className="ui container comments">
+
+      <ApprovalCard>
+        <CommentDetail
+          author ="sam"
+          timeAgo="today at 4:45pm"
+          text="hey there"
+          avatar={faker.image.avatar()}
+          />
+      </ApprovalCard>
+
+      <ApprovalCard>
+      <CommentDetail
+        author ="alex"
+        timeAgo="today at 4:45pm"
+        text="sup dude"
+        avatar={faker.image.avatar()}
+         />
+
+       </ApprovalCard>
+
+       <ApprovalCard>
+         
+       <CommentDetail
+         author ="jane"
+         timeAgo="today at 4:45pm"
+         text="what up dawg"
+         avatar={faker.image.avatar()}
+         />
+        </ApprovalCard>
     </div>
   );
 }
 
 ReactDOM.render(<App />, document.querySelector('#root'));
 
-// http://semantic-ui.com let's install it! 
+// http://semantic-ui.com let's install it!
 // https://cdnjs.com/libraries/semantic-ui
 // https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css
